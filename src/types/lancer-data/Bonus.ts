594f91c5b1424b50ae66b444d65130f995c77cd8
Ben Hirsch <benhirsch42@gmail.com>
2022-08-22 01:47:03 +0100

    bonus id string types

--- src/types/lancer-data/Bonus.ts
+++ src/types/lancer-data/Bonus.ts
@@ -1,7 +1,37 @@
 import { DamageType, RangeType, WeaponSize, WeaponType } from "./enums";
 
+type BonusId =
+  | "ai_cap"
+  | "armor"
+  | "cb_point"
+  | "cheap_stress"
+  | "cheap_struct"
+  | "core_power"
+  | "deployable_hp"
+  | "drone_hp"
+  | "edef"
+  | "evasion"
+  | "heatcap"
+  | "hp"
+  | "license_point"
+  | "limited_bonus"
+  | "mech_skill_point"
+  | "overcharge"
+  | "pilot_armor"
+  | "pilot_edef"
+  | "pilot_evasion"
+  | "pilot_gear"
+  | "pilot_hp"
+  | "pilot_speed"
+  | "range"
+  | "repcap"
+  | "save"
+  | "size"
+  | "skill_point"
+  | "talent_point";
+
 export type Bonus = {
-  id: string;
+  id: BonusId;
   val: string | number | string[];
   damage_types?: DamageType[];
   range_types?: RangeType[];
